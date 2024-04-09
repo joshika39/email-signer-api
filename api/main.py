d07b12c2f360e33a9193fa4b5014f899d4df1660
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-09 17:26:26 +0200

    fixed imports

--- api/main.py
+++ api/main.py
@@ -4,7 +4,10 @@ import sys
 import os
 
 path_root = Path(__file__).parents[1]
+email_path = os.path.join(path_root, 'email')
+
 sys.path.append(os.path.join(path_root))
+sys.path.append(email_path)
 
 from api import routes
 
